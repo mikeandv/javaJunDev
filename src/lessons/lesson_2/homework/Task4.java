2f2c340eaabf38bf2a513dca0c65ff003a3b85c3
Michael <mike.andrews.1988@gmail.com>
2018-10-21 01:09:42 +0300

    new task resolve

--- src/lessons/lesson_2/homework/Task4.java
+++ src/lessons/lesson_2/homework/Task4.java
@@ -1,50 +1,12 @@
 package lessons.lesson_2.homework;
 
+import java.util.Scanner;
+
 public class Task4 {
     public static void main(String[] args) {
-
-        int a = 8;
-        int n = 11;
-        //double res = (Math.pow(a, (p-1)) );
-        //double ff = res % a;
+        Scanner in = new Scanner(System.in);
+        int number = in.nextInt();
 
 
-        for (int i = 0; i < 100; i++) {
-
-
-            if ((a % n) == 0) {
-                continue;
-            }
-            double res = (Math.pow(a, (n-1)) ) % n;
-
-
-            if ( ( (Math.pow(a, (n-1)) ) % n) != 1 ) {
-                System.out.println("Число составное");
-                break;
-
-
-            }
-        }
-//
-//
-//        if ( ( (Math.pow(a, (n-1)) ) % a) != 1 ) {
-//            System.out.println("Число составное");
-//
-//        }
-//
-//        System.out.println(4%497);
-//
-//        int b = 4;
-//        int n = 13;
-//        int m = 497;
-//        int c = 1;
-//
-//
-//        for (int i = 1; i <= n; i++) {
-//
-//            c = (c * b) % m;
-//
-//        }
-//        System.out.println(c);
     }
 }
