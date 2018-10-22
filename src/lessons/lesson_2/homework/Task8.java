08614861ef292d7e2b0f598fc78930b943466789
Michael <mike.andrews.1988@gmail.com>
2018-10-22 09:13:22 +0300

    task8 refactor

--- src/lessons/lesson_2/homework/Task8.java
+++ src/lessons/lesson_2/homework/Task8.java
@@ -4,34 +4,31 @@ public class Task8 {
     public static void main(String[] args) {
 
         //координаты вершины X
-        int verX_x = -5;
-        int verX_y = 1;
+        int verX_x = -3;
+        int verX_y = -3;
 
         //координаты вершины Y
-        int verY_x = 4;
-        int verY_y = 2;
+        int verY_x = -3;
+        int verY_y = -8;
 
         //координаты вершины Z
-        int verZ_x = -4;
-        int verZ_y = -4;
+        int verZ_x = -10;
+        int verZ_y = -3;
 
-        //Вспомогательная точка
-        //int varC = 0;
 
-        //Вычислим длинну отрезков XY, YZ и ZX
-        double sectionXY = Math.abs( Math.sqrt( Math.pow((verY_x - verX_x), 2) + Math.pow((verY_y - verX_y), 2)));
-        double sectionYZ = Math.abs( Math.sqrt( Math.pow((verZ_x - verY_x), 2) + Math.pow((verZ_y - verY_y), 2)));
-        double sectionZX = Math.abs( Math.sqrt( Math.pow((verX_x - verZ_x), 2) + Math.pow((verX_y - verZ_y), 2)));
+        int sectionXY = (int) Math.pow((verY_x - verX_x), 2) + (int) Math.pow((verY_y - verX_y), 2);
+        int sectionYZ = (int) Math.pow((verZ_x - verY_x), 2) + (int) Math.pow((verZ_y - verY_y), 2);
+        int sectionZX = (int) Math.pow((verX_x - verZ_x), 2) + (int) Math.pow((verX_y - verZ_y), 2);
 
-        if ((sectionXY == (Math.sqrt(Math.pow(sectionYZ, 2) + Math.pow(sectionZX, 2))))) {
+        if (sectionXY == (sectionYZ + sectionZX)) {
 
             System.out.println("Треугольник прямоугольный");
 
-        } else if ((sectionYZ == (Math.sqrt(Math.pow(sectionXY, 2) + Math.pow(sectionZX, 2))))) {
+        } else if (sectionYZ == (sectionXY + sectionZX)) {
 
             System.out.println("Треугольник прямоугольный");
 
-        } else if ((sectionZX == (Math.sqrt(Math.pow(sectionYZ, 2) + Math.pow(sectionXY, 2))))) {
+        } else if (sectionZX == (sectionYZ + sectionXY)) {
 
             System.out.println("Треугольник прямоугольный");
 
