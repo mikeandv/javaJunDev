7dc91fbdbc297e9f3e9f3892b9c0f00700afec1f
Michael <mike.andrews.1988@gmail.com>
2018-10-24 10:52:44 +0300

    add comments

--- src/lessons/lesson_3/homework/TaskString3.java
+++ src/lessons/lesson_3/homework/TaskString3.java
@@ -12,22 +12,31 @@ public class TaskString3 {
 
         for (int i = 0; i < stringSt.length(); i++){
 
+
+            //Проверка, что символ является числом
             if (Character.isDigit(stringSt.charAt(i))) {
                 strToArr = String.valueOf(stringSt.charAt(i));
 
+                //Проверка, что следующий символ тоже число
                 int k = i + 1;
 
                 while ( (i != stringSt.length() - 1) && ((Character.isDigit(stringSt.charAt(k))) && (k < stringSt.length() )) ) {
 
+                    //Если число, то плюсуем строки чля получения значения
                     strToArr += String.valueOf(stringSt.charAt(k));
+                    //Инктеменируем k для проверки следующего символа
                     k++;
+                    //Инкрементируем i чтобы чикл проверял следующие значения
                     i++;
                 }
+                //Помещаем значение переменной в элемент массива
                 arr[arrInx] = Integer.parseInt(strToArr);
+                //Инкрементируем счетчик элементов массива для его пересоздания
                 arrInx++;
 
             }
         }
+        //Создаем копию массива, но уже заданной длинны (по количеству элементов)
         int[] copyByRange = Arrays.copyOfRange(arr, 0,arrInx);
 
         System.out.println(Arrays.toString(copyByRange));
