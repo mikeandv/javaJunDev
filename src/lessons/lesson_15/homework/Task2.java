d6fcda943d74d0b9a4a52c7447210da73b90eaa6
Michael <mike.andrews.1988@gmail.com>
2018-12-01 18:40:11 +0300

    refactor

--- src/lessons/lesson_15/homework/Task2.java
+++ src/lessons/lesson_15/homework/Task2.java
@@ -7,62 +7,73 @@ import java.io.*;
 
 public class Task2 {
 
-    public void fileSeparate(File file, long partSize) {
+    public void fileSep2(File file, int size) {
 
-        try(RandomAccessFile inFile = new RandomAccessFile(file.getPath(), "r");
-            RandomAccessFile outFile = new RandomAccessFile(new File("files/" + file.getName().replaceAll("\\.txt", "")+ "_part1.txt").getPath(), "rw");
-            RandomAccessFile outFile2 = new RandomAccessFile(new File("files/" + file.getName().replaceAll("\\.txt", "")+ "_part2.txt").getPath(), "rw")
-        )
-        {
-            StringBuilder sb = new StringBuilder();
+        int part = 1;
 
-            //inFile.seek(partSize);
-            int b = inFile.read();
-            long cnt = 0;
+        byte[] buff = new byte[1024 * size];
 
-            while(cnt < partSize) {
-                sb.append((char) b);
-                b = inFile.read();
-                cnt++;
+        try( InputStream in = new FileInputStream(file);
+             BufferedInputStream buffIn = new BufferedInputStream(in)
+        ) {
+            int len;
+
+            while ( (len = buffIn.read(buff)) > 0) {
+                String[] fileName = file.getName().split("\\.");
+                File newFile = new File(file.getParent(), (fileName[0] + "_part" + part++ + "." + fileName[1]));
+
+                try(OutputStream out = new FileOutputStream(newFile)){
+                    out.write(buff, 0, len);
+                }
+
             }
 
-            byte[] bytes = sb.toString().getBytes();
-            outFile.write(bytes);
-            //+++++++++++++++++++
-
-            StringBuilder sb2 = new StringBuilder();
-            inFile.seek(partSize);
-
-            b = inFile.read();
-
-            while(b != -1) {
-                sb2.append((char) b);
-                b = inFile.read();
-            }
-
-            byte[] bytes2 = sb2.toString().getBytes();
-            outFile2.write(bytes2);
-
-
-
-
-//            byte[] buffer = new byte[1024];
-//            int len;
-//
-//            while ((len = inFile.read(buffer)) > 0) {
-//                outFile.write(buffer, 0, len);
-//
-//            }
-
-
-        } catch (IOException e) {
+        } catch (IOException e){
             System.out.println(e.getMessage());
         }
+
+
+
     }
+
+    public void fileGlue (File[] files, String newName) {
+
+        for(int i = 0; i < files.length; i++) {
+
+            byte[] buff = new byte[1024];
+
+            try (
+                    InputStream in = new FileInputStream(files[i]);
+                    BufferedInputStream buffIn = new BufferedInputStream(in)
+
+            ) {
+                int len;
+
+                while ( (len = buffIn.read(buff)) > 0) {
+                    String[] fileName = files[i].getName().split("\\.");
+                    File newFile = new File(files[i].getParent(), (newName + "." + fileName[1]));
+
+                    try(OutputStream out = new FileOutputStream(newFile, true)){
+                        out.write(buff, 0, len);
+                    }
+
+                }
+
+            } catch (IOException e){
+                System.out.println(e.getMessage());
+            }
+
+        }
+    }
+
 
     public static void main(String[] args) {
 
         Task2 t = new Task2();
-        t.fileSeparate(new File("files/"+ StInput.read()), (long) NumIn.read());
+        t.fileSep2(new File("files/" + StInput.read()), NumIn.read());
+
+        File[] f = {new File("files/"+ StInput.read()), new File("files/"+ StInput.read()), new File("files/"+ StInput.read())};
+        t.fileGlue(f, StInput.read());
+
     }
 }
